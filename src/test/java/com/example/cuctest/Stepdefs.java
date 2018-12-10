7573e75ffea6a31e4df8113bff260e1009db4842
testuser <testuser@win2008r2>
2018-12-10 14:42:04 -0500

    Add the missing test cases

--- src/test/java/com/example/cuctest/Stepdefs.java
+++ src/test/java/com/example/cuctest/Stepdefs.java
@@ -1,8 +1,23 @@
 package com.example.cuctest;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class Stepdefs {
+	@When("^I wait (\\d+) hour$")
+	public void i_wait_hour(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^my belly should growl$")
+	public void my_belly_should_growl() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
     @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
